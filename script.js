a0d4c904415d1a6451c3ad297a57cbe1b1cb185c
Sergey <Zismantt@gmail.com>
2022-11-01 20:55:19 +0200

    list

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-let a = {};
-let b = {};
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('#input'),
+          btn = document.querySelector('#btn'),
+          ul = document.querySelector('ul');
+       
+
+    
+    
+          
+
+    btn.addEventListener('click', () => {
+        if (!input.value) {return;}
+ 
+        console.log(input.value);
+        render(input.value);
+        input.value = '';
+       
+    });
+
+
+    function render(inf) {
+           let li = document.createElement('li');
+           li.innerHTML = `<div class='item'>${inf} <button>del</button></div>`;
+           ul.append(li);
+
+           
+        }
+
+
+    
+    
+
+
+
+
+
+
+
+});
