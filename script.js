9d9b2b82718af1042badc6d07d8c2301123a87c6
Zismant <100025984+Zismant@users.noreply.github.com>
2022-11-02 11:18:26 +0200

    listLength

--- script.js
+++ script.js
@@ -1,48 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => {
     const input = document.querySelector('#input'),
           btn = document.querySelector('#btn'),
-          ul = document.querySelector('ul');
+          ul = document.querySelector('ul'),
+          doListLength = document.querySelector('span'),
+          data = [];
        
-
-    
-    
-          
 
     btn.addEventListener('click', () => {
         if (!input.value) {return;}
- 
-        
-        render(input.value);
+
+
+        data.push({dooo: input.value, check: false});
+        render();
         input.value = '';
+        
        
     });
 
 
-    function render(inf) {
+    function render() {
+        console.log(data);
+        doListLength.textContent = data.length;
+        const uList = document.querySelectorAll('li');
+
+        uList.forEach((item => item.remove()));
+
+        data.forEach((item, i) => {
+       
            let li = document.createElement('li');
            let btn = document.createElement("button");
            let checkBox = document.createElement('input');
            checkBox.type = 'checkbox';
-           btn.textContent = 'del   '; 
-           li.innerHTML = `<div class='item'>${inf}</div>`;
-           
-           ul.append(li);
-           li.firstChild.firstChild.after(btn);
-           li.firstChild.firstChild.after(checkBox);
-           
+           checkBox.checked = item.check;
+           btn.textContent = 'del';
+           item.check ? li.classList.add('remove') : li.classList.remove('remove');
 
+            li.innerHTML = `<div class='item'>${item.dooo}</div>`;
            
-
-           btn.addEventListener('click', () => {
-                if(checkBox.checked) {
-                li.remove(); }  
-           });
-
-           checkBox.addEventListener('click', () => {
-            li.classList.toggle('remove');
+            ul.append(li);
+            li.firstChild.firstChild.after(btn);
+            li.firstChild.firstChild.after(checkBox);
             
-       });
-        
+ 
+            btn.addEventListener('click', () => {
+                 if(item.check) {
+                 data.splice(i, 1); }  
+                 render();
+            });
+ 
+            checkBox.addEventListener('click', () => {
+                item.check = !item.check;
+                
+                render();
+            });
+         
+
+        });
+
+       
            
         
 
